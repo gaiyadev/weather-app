8c15fa4715fc38c54bd98f1139d7259272a499f8
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-07-03 18:09:03 +0100

    add: added weather icons

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import InputField from '../../components/TextFields/InputField';
+import '../../css/weather-icons.min.css';
 
 const API_Key = '7d1d5e1b3e6ff7d9cbac6936b2f277e5';
 class Weather extends React.Component {
@@ -10,10 +10,27 @@ class Weather extends React.Component {
     render() {
         return (
             <div className="container">
-                <InputField />
+                <div className="cards">
+                    <h3>London</h3>
+                    <h5 className="py-4" >
+                        <i class="wi wi-day-sunny display-1" ></i>.
+                    </h5>
+                    <h1 className="py-2">25&deg; </h1>
+                    {/* calling the minAndMAx function */}
+                    {minAndMax_Temp(24, 19)}
+                </div>
             </div>
         );
     }
 }
 
+
+const minAndMax_Temp = (min, max) => {
+    return (
+        <h3 >
+            <span className="px-4">{min}&deg;</span>
+            <span className="px-4">{max}&deg;</span>
+        </h3>
+    );
+}
 export default Weather;
